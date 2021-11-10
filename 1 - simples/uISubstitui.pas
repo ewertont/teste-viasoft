e15b562bff5d281d26eee01b809ed36278d81ad5
Ewerton Tavares <ewertont@users.noreply.github.com>
2021-11-10 19:49:27 -0300

    update

---
+++ 1 - simples/uISubstitui.pas
@@ -0,0 +1,15 @@
+unit uISubstitui;
+
+interface
+
+type
+
+  ISubstitui = interface
+  ['{FC37EFA8-4F11-4AF7-AF47-AE7F1C747A29}']
+    function Substituir(Str, Velho, Novo: String): String;
+  end;
+
+implementation
+
+end.
+
